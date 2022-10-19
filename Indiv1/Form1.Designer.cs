e1a9b950f8a9921a8c269fe24facc8ed33720bd6
josephjoguts <josephjoguts@yandex.ru>
2022-10-19 12:33:40 +0300

    Initial commit

---
+++ Indiv1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Indiv1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button clear;
+            this.canvasBox = new System.Windows.Forms.PictureBox();
+            this.start = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize) (this.canvasBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // clear
+            // 
+            clear.Location = new System.Drawing.Point(40, 424);
+            clear.Name = "clear";
+            clear.Size = new System.Drawing.Size(116, 48);
+            clear.TabIndex = 2;
+            clear.Text = "clear";
+            clear.UseVisualStyleBackColor = true;
+            clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // canvasBox
+            // 
+            this.canvasBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canvasBox.Location = new System.Drawing.Point(1, 0);
+            this.canvasBox.Name = "canvasBox";
+            this.canvasBox.Size = new System.Drawing.Size(798, 396);
+            this.canvasBox.TabIndex = 0;
+            this.canvasBox.TabStop = false;
+            this.canvasBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.canvasBox_MouseClick);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(298, 424);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(212, 48);
+            this.start.TabIndex = 1;
+            this.start.Text = "generate";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 484);
+            this.Controls.Add(clear);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.canvasBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize) (this.canvasBox)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.PictureBox canvasBox;
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button clear;
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        #endregion
+    }
+}
---
+++ Indiv1/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Indiv1
+{
+    public partial class Form1 : Form
+    {
+        private Graphics canvas;
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        Pen blackPen = new Pen(Color.Black, 3);
+        List<Point> points = new List<Point>();
+        public Form1()
+        {
+            InitializeComponent();
+            canvas = canvasBox.CreateGraphics();
+            
+        }
+
+        private void canvasBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            canvas.FillEllipse(blackBrush,e.X-3,e.Y-3,7,7);
+            points.Add(e.Location);
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            start.Enabled = false;
+            clear.Enabled = false;
+            List<Point> hull = new List<Point>();
+            Point vPointOnHull = points.Where(p => p.X == points.Min(min => min.X)).First();
+            
+            Point vEndpoint;
+            do
+            {
+                hull.Add(vPointOnHull);
+                vEndpoint = points[0];
+
+                for (int i = 1; i < points.Count; i++)
+                {
+                    if ((vPointOnHull == vEndpoint)
+                        || (Orientation(vPointOnHull, vEndpoint, points[i]) == -1))
+                    {
+                        vEndpoint = points[i];
+                    }
+                }
+                canvas.DrawLine(blackPen, vPointOnHull, vEndpoint);
+                Thread.Sleep(1000);
+                vPointOnHull = vEndpoint;
+
+            }
+            while (vEndpoint != hull[0]);
+            start.Enabled = true;
+            clear.Enabled = true;
+        }
+
+        private static int Orientation(Point p1, Point p2, Point p)
+        {
+            // Determinant
+            int Orin = (p2.X - p1.X) * (p.Y - p1.Y) - (p.X - p1.X) * (p2.Y - p1.Y);
+
+            if (Orin > 0)
+                return -1; //          (* Orientation is to the left-hand side  *)
+            if (Orin < 0)
+                return 1; // (* Orientation is to the right-hand side *)
+
+            return 0; //  (* Orientation is neutral aka collinear  *)
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            canvasBox.Image = new Bitmap(1300, 900);
+            points.Clear();
+        }
+    }
+}
